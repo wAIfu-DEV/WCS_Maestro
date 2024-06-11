0f9d02ee28a84538b315071682a69fa3f2141b68
wAIfu-DEV <w.ai.fu.dev.team@gmail.com>
2024-06-11 14:17:23 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,292 @@
+/**
+ * @name WCS-Maestro
+ * @author w-AI-fu_DEV
+ * @description AI Vtuber Collab Orchestration
+ */
+
+const fs = require("fs");
+const wcc = require("./wcc");
+const openai = require("openai");
+
+let ENV = {
+    URL: "",
+    USER: "",
+    PASS: "",
+    ROOM: "",
+    OPENAI_KEY: "",
+};
+
+const MAX_BACKLOG_AMOUNT = 10;
+const SYS_PROMPT =
+    "# Dialogue Orchestration\n" +
+    "## Goal\n" +
+    "Given a dialogue between a variable number of people, " +
+    "return the name of the person the last message " +
+    "is destined to, or the name of person who *should* " +
+    "respond to the last message.\n" +
+    "The provided name should never be the same as the person who sent the " +
+    "last message.\n" +
+    "## Response\n" +
+    "Your response should only contain the name of the chosen person and " +
+    "nothing else. No other commentary needed.\n";
+
+/** @type { wcc.ProtocolMessage[] } */
+let dialogue_backlog = [];
+
+/** @typedef { { user: string, name?: string | null } } Participant */
+
+/** @type { Participant[] } */
+let known_participants = [];
+
+readEnvFile();
+
+let openai_client = new openai.OpenAI({ apiKey: ENV.OPENAI_KEY });
+let client = new wcc();
+
+function log(...args) {
+    console.log("MAESTRO:", ...args);
+}
+
+function recursiveFetchParticipants() {
+    getParticipants().then((arr) => {
+        arr.forEach((user) => {
+            if (user == ENV.USER) return;
+
+            let is_known = false;
+            known_participants.forEach((v) => {
+                if (v.user == user) {
+                    is_known = true;
+                    return;
+                }
+            });
+            if (!is_known) {
+                known_participants.push({
+                    user: user,
+                    name: null,
+                });
+            }
+        });
+    });
+    setTimeout(recursiveFetchParticipants, 15_000);
+}
+
+/**
+ * @returns { Promise<string[]> }
+ */
+async function getParticipants() {
+    let url = new URL(ENV.URL);
+    url.protocol = "http";
+    url.port = "5000";
+    url.pathname = "/Websocket";
+    url.search = `roomId=${ENV.ROOM}`;
+
+    log("Fetching users...");
+
+    try {
+        var resp = await fetch(url, {
+            headers: {
+                Accept: "application/json",
+            },
+        });
+    } catch {
+        log("Error when contacting server for user data.");
+        return [];
+    }
+
+    try {
+        var json = await resp.json();
+    } catch {
+        log("Error when parsing server user data.");
+        return [];
+    }
+
+    if (!Array.isArray(json)) {
+        log("Received wrong user data from server.");
+        return [];
+    }
+
+    log("Users:", json);
+
+    return json;
+}
+
+/**
+ * @returns { void }
+ */
+function readEnvFile() {
+    fs.readFileSync("./.env", { encoding: "utf8" })
+        .split(/\r\n|\n/g)
+        .forEach((v) => {
+            if (v.trim() == "") return;
+            let arr = v.split("=");
+            ENV[arr[0]] = arr.slice(1, undefined).join("=");
+        });
+}
+
+/**
+ * @param { string } sender
+ * @param { string } content
+ * @param { wcc.ProtocolMessage } json
+ */
+async function handleIncomingText(sender, content, json) {
+    log("Incoming:", sender, ":", content);
+
+    let is_known = false;
+
+    for (let participant of known_participants) {
+        if (participant.user == json.from) {
+            is_known = true;
+            participant.name = sender;
+            break;
+        }
+    }
+
+    if (!is_known) {
+        known_participants.push({
+            user: json.from,
+            name: sender,
+        });
+    }
+
+    let target = await decideTarget(json);
+    let target_user = null;
+
+    let target_known = false;
+
+    for (let participant of known_participants) {
+        if (participant.user == target) {
+            target_known = true;
+            target_user = participant.user;
+            break;
+        }
+        if (participant.name == target) {
+            target_known = true;
+            target_user = participant.user;
+            break;
+        }
+    }
+
+    if (!target_known) {
+        // Invalid target, dispatch to random known participant
+        log("Failed to find target, picking random one.");
+
+        target_user =
+            known_participants[randUpTo(known_participants.length)].user;
+
+        while (target_user == json.from) {
+            target_user =
+                known_participants[randUpTo(known_participants.length)].user;
+        }
+
+        target = target_user;
+    }
+
+    if (!target_user) {
+        log("Critical: could not find user to send message to.");
+        return;
+    }
+
+    log("Sending to:", target);
+
+    client.sendText(sender, content, [target_user]);
+}
+
+/**
+ * @param { wcc.ProtocolMessage } json
+ * @returns { void }
+ */
+function appendToBacklog(json) {
+    while (dialogue_backlog.length > MAX_BACKLOG_AMOUNT)
+        dialogue_backlog.shift();
+    dialogue_backlog.push(json);
+}
+
+/**
+ * @returns { openai.OpenAI.ChatCompletionMessageParam[] }
+ */
+function getBacklogAsMessages() {
+    let result = [];
+    for (let json of dialogue_backlog) {
+        result.push({
+            role: "user",
+            name: json.payload.name,
+            content: `${json.payload.name}: ${json.payload.content.trim()}`,
+        });
+    }
+    return result;
+}
+
+/**
+ * @returns { string }
+ */
+function getSystemPrompt() {
+    return (
+        SYS_PROMPT +
+        "## Known Participants\n" +
+        known_participants
+            .map((v) => {
+                return !v.name ? v.user : v.name;
+            })
+            .join(", ")
+    );
+}
+
+/**
+ * @param { wcc.ProtocolMessage } json
+ * @returns { Promise<string> }
+ */
+async function decideTarget(json) {
+    appendToBacklog(json);
+    let prompt_messages = [
+        {
+            role: "system",
+            content: getSystemPrompt(),
+        },
+        ...getBacklogAsMessages(),
+    ];
+
+    //log("Prompt:", prompt_messages);
+
+    let completion_promise = openai_client.chat.completions.create({
+        messages: prompt_messages,
+        model: "gpt-3.5-turbo",
+        max_tokens: 100,
+        temperature: 1.0,
+        stream: false,
+    });
+
+    try {
+        var completion = await completion_promise;
+    } catch {
+        log("Error while contacting OpenAI.");
+        return "";
+    }
+
+    return completion.choices[0].message.content ?? "";
+}
+
+/**
+ * @param { number } x
+ * @returns { number }
+ */
+function randUpTo(x) {
+    return Math.floor(Math.random() * x);
+}
+
+async function main() {
+    client
+        .connect(ENV.URL, ENV.ROOM, { user: ENV.USER, pass: ENV.PASS })
+        .then(() => {
+            client.onTextMessage = handleIncomingText;
+            recursiveFetchParticipants();
+        })
+        .catch((reason) => {
+            log("Failed to connect to server. Reason:", reason);
+        });
+
+    while (true) {
+        await new Promise((resolve) => setTimeout(resolve, 250));
+    }
+}
+
+main();
---
+++ wcc.js
@@ -0,0 +1,519 @@
+"use strict";
+
+const ws = require("ws");
+
+/**
+ * Throws an error if condition is false.
+ * @private
+ * @param { boolean } condition
+ * @param { string } message
+ */
+function assert(condition, message) {
+    if (!condition) throw new Error("WCC: " + message);
+}
+
+/**
+ * Log to console with prefix
+ * @private
+ * @param {...any} args
+ */
+function wccLog(...args) {
+    console.log("WCC:", ...args);
+}
+
+/**
+ * Log to console with prefix
+ * @private
+ * @param {...any} args
+ */
+function wccWarn(...args) {
+    console.warn("WCC:", ...args);
+}
+
+/**
+ * Log to console with prefix
+ * @param {...any} args
+ */
+function wccError(...args) {
+    console.trace("WCC:", ...args);
+}
+
+//------------------------------------------------------------------------------
+// TYPEDEFS
+//------------------------------------------------------------------------------
+/** @typedef {{ user: string, pass: string }} UserPass */
+/** @typedef { "message" | "data" | "other" } ProtocolMessageType */
+/** @typedef {{ name: string, content: string }} Payload */
+/** @typedef {{ version: number, type: ProtocolMessageType, from: string, to: string[], payload: unknown }} ProtocolMessageUnknown */
+/** @typedef {{ version: number, type: "message", from: string, to: string[], payload: Payload }} ProtocolMessage */
+
+//------------------------------------------------------------------------------
+// CLIENT
+//------------------------------------------------------------------------------
+class WebsocketCollabClient {
+    //--------------------------------------------------------------------------
+    // MEMBERS
+    //--------------------------------------------------------------------------
+    /**
+     * Version of the protocol used for the communication from client to client.
+     * @constant
+     * @readonly
+     * @type { number }
+     */
+    PROTOCOL_VERSION = 1;
+
+    /**
+     * If the client is currently connected to the server.
+     * @readonly
+     * @type { boolean }
+     */
+    connected = false;
+
+    /**
+     * ID of the joined channel.
+     * @readonly
+     * @type { string }
+     */
+    channel_id = "";
+
+    /**
+     * Url of the joined server.
+     * @readonly
+     * @type { URL? }
+     */
+    server_url = null;
+
+    /**
+     * @private
+     * @type { string }
+     */
+    user = "";
+
+    /**
+     * @private
+     * @type { ws? }
+     */
+    socket = null;
+
+    /**
+     * @private
+     * @type { number }
+     */
+    retry_count = 0;
+
+    /**
+     * @private
+     * @constant
+     * @readonly
+     * @type { number }
+     */
+    MAX_RETRIES = 5;
+
+    //--------------------------------------------------------------------------
+    // PRIVATE METHODS
+    //--------------------------------------------------------------------------
+    /**
+     * @private
+     * @param { ws.ErrorEvent } ev
+     */
+    handleWsError(ev) {
+        wccError("Collab websocket error:", ev.type, ev.message);
+        wccWarn(
+            "Lost connection to Collab Websocket due to error, trying reconnect..."
+        );
+        this.reconnect();
+    }
+
+    /**
+     * @private
+     * @param { ws.CloseEvent } ev
+     */
+    handleWsClose(ev) {
+        if (!ev.wasClean) {
+            wccWarn("Lost connection to Collab Websocket, trying reconnect...");
+            this.reconnect();
+            return;
+        }
+    }
+
+    /**
+     * @private
+     * @param { ws.MessageEvent } ev
+     */
+    handleWsMessage(ev) {
+        let data = ev.data.toString("utf8");
+
+        /** @type { unknown | undefined } */
+        let obj;
+        try {
+            obj = JSON.parse(data);
+        } catch {
+            return;
+        }
+
+        if (!this.isWellFormedProtocolMessage(obj)) {
+            return;
+        }
+
+        /** @type { ProtocolMessageUnknown } */
+        let proto_msg = obj;
+
+        if (!this.isWellFormedMessagePayload(proto_msg.payload)) {
+            return;
+        }
+
+        /** @type { ProtocolMessage } */
+        let msg = proto_msg;
+
+        if (this.onAllMessages) {
+            this.onAllMessages(msg);
+        }
+
+        if (!(msg.to.includes("all") || msg.to.includes(this.user))) {
+            // Should not be received by user.
+            return;
+        }
+
+        if (msg.from == this.user) {
+            // Is from user
+            return;
+        }
+
+        switch (msg.type) {
+            case "message": {
+                if (!this.onTextMessage) return;
+                this.onTextMessage(msg.payload.name, msg.payload.content, msg);
+                return;
+            }
+            case "data": {
+                if (!this.onDataMessage) return;
+                this.onDataMessage(msg.payload.name, msg.payload.data, msg);
+                return;
+            }
+        }
+    }
+
+    /**
+     * @private
+     * @param { URL } url
+     * @returns { Promise<void> }
+     */
+    connectWebsocket(url) {
+        return new Promise((resolve, reject) => {
+            let resolved = false;
+
+            this.socket = new ws(url);
+
+            this.socket.onerror = (err) => {
+                wccError(
+                    "Error while trying to connect to server:",
+                    err.type,
+                    err.message
+                );
+                if (resolved) return;
+                resolved = true;
+                reject(err.message);
+            };
+
+            this.socket.onopen = (ev) => {
+                this.socket.ping = this.socket.pong();
+                this.socket.onerror = this.handleWsError.bind(this);
+                this.socket.onmessage = this.handleWsMessage.bind(this);
+                this.socket.onclose = this.handleWsClose.bind(this);
+                this.connected = true;
+                this.retry_count = 0;
+                if (resolved) return;
+                resolved = true;
+                resolve();
+            };
+        });
+    }
+
+    /**
+     * @private
+     * @param { unknown } obj
+     * @returns { boolean }
+     */
+    isWellFormedProtocolMessage(obj) {
+        if (obj.version == undefined || typeof obj.version != "number")
+            return false;
+        if (obj.type == undefined || typeof obj.type != "string") return false;
+        if (obj.from == undefined || typeof obj.from != "string") return false;
+        if (obj.to == undefined || typeof obj.to != "object") return false;
+        if (obj.to.length == undefined) return false;
+        if (obj.payload == undefined) return false;
+        return true;
+    }
+
+    /**
+     * @private
+     * @param { unknown } payload
+     * @returns { boolean }
+     */
+    isWellFormedMessagePayload(payload) {
+        if (payload.name == undefined || typeof payload.name != "string")
+            return false;
+        if (payload.content == undefined || typeof payload.content != "string")
+            return false;
+        return true;
+    }
+
+    /**
+     * @private
+     */
+    clearServerData() {
+        this.user = "";
+        this.channel_id = "";
+        this.server_url = null;
+        this.connected = false;
+    }
+
+    /**
+     * @private
+     */
+    reconnect() {
+        if (this.retry_count >= this.MAX_RETRIES) {
+            wccError(
+                "Max retries reached. Could not reconnect to the Websocket Collab server."
+            );
+            this.clearServerData();
+            return;
+        }
+
+        let delay_sec = Math.pow(2, this.retry_count) * 1000;
+        wccLog(`Reconnecting in ${delay_sec / 1000} seconds...`);
+        this.retry_count++;
+
+        if (this.socket) {
+            this.socket.removeAllListeners();
+            this.socket.close();
+            this.socket = null;
+        }
+
+        setTimeout(() => {
+            this.connectWebsocket(this.server_url)
+                .then(() => {
+                    wccLog("Reconnected successfully.");
+                })
+                .catch((err) => {
+                    wccError("Reconnection attempt failed:", err);
+                    this.reconnect(); // Retry again
+                });
+        }, delay_sec);
+    }
+
+    //--------------------------------------------------------------------------
+    // PUBLIC METHODS
+    //--------------------------------------------------------------------------
+    /**
+     * Connects to the server and joins the selected channel.
+     *
+     * Rejects if fails to establish connection to server.
+     * @method
+     * @function
+     * @param { string | URL } url Url to the collab server.
+     * @param { string } channel_id ID of the channel to join.
+     * @param { UserPass } auth User and Password.
+     * @returns { Promise<void> }
+     */
+    connect(url, channel_id, auth) {
+        assert(url, "argument url cannot be undefined.");
+        assert(channel_id, "argument channel_id cannot be undefined.");
+        assert(auth, "argument auth cannot be undefined.");
+        assert(auth.user, "argument auth.user cannot be undefined.");
+        assert(auth.pass, "argument auth.pass cannot be undefined.");
+
+        assert(
+            url instanceof URL || typeof url == "string",
+            "argument url must be of type string or URL."
+        );
+        assert(
+            typeof channel_id == "string",
+            "argument channel_id must be of type string."
+        );
+        assert(
+            typeof auth.user == "string",
+            "argument auth.user must be of type string."
+        );
+        assert(
+            typeof auth.pass == "string",
+            "argument auth.user must be of type string."
+        );
+
+        let uri;
+        if (typeof url != "string") uri = url;
+        else uri = new URL(url);
+
+        uri.username = auth.user;
+        uri.password = auth.pass;
+        uri.pathname = `/${channel_id}`;
+
+        this.user = auth.user;
+        this.server_url = uri;
+        this.channel_id = channel_id;
+
+        return this.connectWebsocket(uri);
+    }
+
+    /**
+     * Closes the connection to the server.
+     * @method
+     * @function
+     */
+    disconnect() {
+        wccLog("Disconnected from Websocket Collab Server.");
+        if (this.socket.readyState != ws.CLOSED) {
+            this.socket.close();
+        }
+    }
+
+    /**
+     * Send a message to users of the channel, with the assumption that they sould
+     * respond to said message.
+     * @method
+     * @function
+     * @param { string } sender_name Name of the sender, for ex: "Hilda" or "Meteora"
+     * @param { string } content Content of the text message.
+     * @param { string[] } to Users to send the message to. If contains `"all"`,
+     * will send message to all participants.
+     */
+    sendText(sender_name, content, to = ["all"]) {
+        assert(
+            sender_name != undefined && typeof sender_name == "string",
+            "argument sender_name must be a string."
+        );
+        assert(
+            content != undefined && typeof content == "string",
+            "argument content must be a string."
+        );
+        assert(
+            to != undefined && typeof to == "object",
+            "argument to must be of type string array."
+        );
+
+        assert(
+            to.length != undefined,
+            "argument to must be of type string array."
+        );
+        for (let name of to) {
+            assert(
+                name != undefined && typeof name == "string",
+                'in argument to: "' + name + '" must be of type string.'
+            );
+        }
+
+        if (!(this.socket != null && this.socket.readyState == ws.OPEN)) {
+            wccWarn("Cannot send to collab, connection to server is closed.");
+            return;
+        }
+
+        /** @type { ProtocolMessage } */
+        let obj = {
+            version: 1,
+            type: "message",
+            from: this.user,
+            to: to,
+            payload: {
+                name: sender_name,
+                content: content,
+            },
+        };
+
+        this.socket.send(JSON.stringify(obj));
+    }
+
+    /**
+     * Send data to other participants, with the assumption that they may or may not
+     * respond to receiving data.
+     * @method
+     * @function
+     * @param { string } label Identifier of the sent data.
+     * @param { string } data Sent data.
+     * @param { string } to Users to send the message to. If contains `"all"`,
+     * will send message to all participants.
+     */
+    sendData(label, data, to = ["all"]) {
+        assert(
+            label != undefined && typeof label == "string",
+            "argument label must be a string."
+        );
+        assert(
+            data != undefined && typeof data == "string",
+            "argument data must be a string."
+        );
+        assert(
+            to != undefined && typeof to == "object",
+            "argument to must be of type string array."
+        );
+
+        assert(
+            to.length != undefined,
+            "argument to must be of type string array."
+        );
+        for (let name of to) {
+            assert(
+                name != undefined && typeof name == "string",
+                'in argument to: "' + name + '" must be of type string.'
+            );
+        }
+
+        if (!(this.socket != null && this.socket.readyState == ws.OPEN)) {
+            wccWarn("Cannot send to collab, connection to server is closed.");
+            return;
+        }
+
+        /** @type { ProtocolMessage } */
+        let obj = {
+            version: 1,
+            type: "data",
+            from: this.user,
+            to: to,
+            payload: {
+                name: label,
+                content: data,
+            },
+        };
+
+        this.socket.send(JSON.stringify(obj));
+    }
+
+    /**
+     * Called when a protocol message is received from the server.
+     *
+     * Is called before `onTextMessage` and `onDataMessage`, so they might also
+     * get called for the same message.
+     *
+     * Only use if you want to handle specific cases.
+     *
+     * Use `onMessage` instead if you are only interested with the text messages.
+     * @member
+     * @type { ((json: ProtocolMessage) => void)? }
+     */
+    onAllMessages = null;
+
+    /**
+     * Called when a text message is received from the server.
+     *
+     * The assumption is that you will respond to this message.
+     * @method
+     * @type { ((sender: string, content: string, raw_json: ProtocolMessage) => void)? }
+     */
+    onTextMessage = null;
+
+    /**
+     * Called when a data message is received from the server.
+     *
+     * The assumption is that you will not respond to this message.
+     * @method
+     * @type { ((label: string, data: string, raw_json: ProtocolMessage) => void)? }
+     */
+    onDataMessage = null;
+
+    removeAllListeners() {
+        this.onAllMessages = null;
+        this.onTextMessage = null;
+        this.onDataMessage = null;
+    }
+}
+
+module.exports = WebsocketCollabClient;
